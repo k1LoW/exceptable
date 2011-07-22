74e3e0e96440ab74cb7135bfb67458a91641e7b2
k1LoW <k1lowxb@gmail.com>
2011-07-22 20:35:02 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Exceptable plugin for CakePHP #
+
+`Exceptable any fields form conditions' plugin for CakePHP.
+
+## Background ##
+
+This plugin can specify fields you want exclude from conditions.
+
+## Installation ##
+
+1. Download this: http://github.com/k1LoW/exceptable/zipball/master
+2. Unzip that download.
+3. Copy the resulting folder to app/plugins
+4. Rename the folder you just copied to exceptable
+5. Add the following code in app_model.php
+
+## Usage ##
+
+    $this->Post->find('first', array('except' => 'Post.modified'));
+
+## License ##
+
+under MIT Lisence
---
+++ models/behaviors/exceptable.php
@@ -0,0 +1,85 @@
+<?php
+class ExceptableBehavior extends ModelBehavior {
+
+    public $settings = array();
+
+    protected $_defaults = array();
+
+    public function setup(Model $model, $config = array()) {
+        $this->settings[$model->alias] = array_merge($this->_defaults, $config);
+    }
+
+    /**
+     * beforeFind
+     *
+     * @return
+     */
+    function beforeFind(Model $model, $queryData){
+        if (!empty($queryData['fields'])) {
+            return $queryData;
+        }
+        if ($queryData['except']) {
+            $except = (array)$queryData['except'];
+            foreach ($except as $key => $field) {
+                if (!preg_match('/^`/', $field)) {
+                    $field = '`' . $field . '`';
+                    if (strpos($field, '.') !== false) {
+                        $field = str_replace('.', '`.`', $field);
+                    }
+                }
+                if (strpos($field, '.') === false) {
+                    $field = '`' . $model->alias . '`.' . $field;
+                }
+                $except[$key] = $field;
+            }
+
+            if (!$db =& ConnectionManager::getDataSource($model->useDbConfig)) {
+                return false;
+            }
+
+            $queryData = $db->__scrubQueryData($queryData);
+
+            $recursive = $model->recursive;
+            if ($recursive === null && isset($queryData['recursive'])) {
+                $recursive = $queryData['recursive'];
+            }
+
+            if (!is_null($recursive)) {
+                $_recursive = $model->recursive;
+                $model->recursive = $recursive;
+            }
+
+            if (!empty($queryData['fields'])) {
+                $db->__bypass = true;
+                $queryData['fields'] = $db->fields($model, null, $queryData['fields']);
+            } else {
+                $queryData['fields'] = $db->fields($model);
+            }
+
+            $_associations = $model->__associations;
+
+            if ($model->recursive == -1) {
+                $_associations = array();
+            } else if ($model->recursive == 0) {
+                unset($_associations[2], $_associations[3]);
+            }
+
+            foreach ($_associations as $type) {
+                foreach ($model->{$type} as $assoc => $assocData) {
+                    $linkModel =& $model->{$assoc};
+                    $external = isset($assocData['external']);
+                    if ($model->useDbConfig == $linkModel->useDbConfig) {
+                        if (true === $db->generateAssociationQuery($model, $linkModel, $type, $assoc, $assocData, $queryData, $external, $null)) {
+                            $linkedModels[$type . '/' . $assoc] = true;
+                        }
+                    }
+                }
+            }
+
+            $queryData['fields'] = array_diff($queryData['fields'], $except);
+        }
+
+        return $queryData;
+    }
+}
+
